2792b9ad36967d200bd0ec181afd27236f01e320
Derek <derekr@me.com>
2015-01-26 20:38:45 -0800

    Adds require prefix and fix call to compiler

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 var through = require('through');
-var riotCompiler = require('riot/compiler');
+var riotCompiler = require('riot/compiler/compile');
 
 module.exports = function (file, o) {
   var opts = o;
   var content = '';
+  var reqriot = 'var riot = require(\'riot\');';
 
   return !file.match(/\.tag$/) ? through() : through(
     function (chunk) { // write
       content += chunk.toString();
     },
     function () { // end
-      this.queue(riotCompiler(content, opts));
+      this.queue(reqriot + riotCompiler(content, opts));
       this.emit('end');
     }
   );
