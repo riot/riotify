22538acc780eea78820046063c47b3f24bf5ee22
Tianxiang Chen <txchendev@gmail.com>
2015-02-14 12:56:23 -0800

    add unittest to cover --ext option, bump version
    Also update the change log and README.md

--- test/compile.js
+++ test/compile.js
@@ -38,3 +38,38 @@ test('compile', function (t) {
     t.ok(out[1].source.match(/riot.tag\(.*todo/), 'compiled compile.tag');
   }));
 });
+
+test('compile-custom-ext', function (t) {
+  var file = path.join(__dirname, 'customext.html');
+  var p = moduleDeps();
+
+  p.write({ transform: riotify, options: {ext: "html"} });
+  p.write({ file: file, id: file, entry: true });
+  p.end();
+
+  t.plan(6);
+  p.pipe(concat(function (out) {
+    t.is(out.length, 2, 'got two files as output');
+    t.ok(out[0].id.match(/riot\.js$/), 'out.0 is riot.js');
+    t.ok(out[0].source.match(/module.exports/), 'riot.js');
+    t.ok(out[1].id.match(/customext\.html$/), 'out.1 is customext.html');
+    t.ok(out[1].source.match(/var riot = require\('riot'\);/), 'require riot');
+    t.ok(out[1].source.match(/riot.tag\(.*customext/), 'compiled compile.tag');
+  }));
+});
+
+test('compile-custom-ext-ignore', function (t) {
+  var file = path.join(__dirname, 'todo.tag');
+  var p = moduleDeps();
+
+  p.write({ transform: riotify, options: {ext: "html"} });
+  p.write({ file: file, id: file, entry: true });
+  p.end();
+
+  t.plan(3);
+  p.pipe(concat(function (out) {
+    t.ok(out[0], 'got output element');
+    t.ok(out[0].source.length, 'got output element with source');
+    t.ok(out[0].source.match(/^<todo>/), 'skipped todo.tag');
+  }));
+});
