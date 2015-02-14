05cd8674e45f08ac60a63e527ba2465072228452
Tianxiang Chen <txchendev@gmail.com>
2015-02-14 08:58:51 -0800

    support specify --ext when invoke riot
    latest riot supports custom extension, like --ext html. Change the regex logic here to support that.

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ var preamble = "var riot = require('riot');\n";
 module.exports = function (file, o) {
   var opts = o;
   var content = '';
+  var ext = o.ext || 'tag';
 
-  return !file.match(/\.tag$/) ? through() : through(
+  return !file.match('\.' + ext + '$') ? through() : through(
     function (chunk) { // write
       content += chunk.toString();
     },
